5a4ce6fe3aef99e21cbedeaf17eb0b99a3af52a4
Deeka0 <62205832+Deeka0@users.noreply.github.com>
2023-09-24 21:13:11 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,13 @@
-import csv, time, os, pathlib
-from __init__ import platform
+from __init__ import *
+import csv
 
 
-path = str(pathlib.Path(__file__).parent.resolve())
-# print(os.path.abspath(__file__))
 
 
 if platform == "darwin":
-    path = path + "/"
+    path = runtime_path + "/"
 
-    command_osx = '''
+    prompt_command = '''
     echo "How many users would you want?"
     read users
     for i in $(seq $users)
@@ -17,41 +15,33 @@ if platform == "darwin":
         echo ${x}
         osascript -e 'tell application "Terminal" to activate' \
         -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
-        -e 'tell application "Terminal" to do script "python3 {y}meet.py" in selected tab of the front window'
-        sleep 2
+        -e 'tell application "Terminal" to do script "python3 {y}meet.py" in selected tab of the front windosleep 2
 
     done'''.format(x = r"{BASH_VERSION}", y = path)
-    
-    command = command_osx
-    clearscreen = "clear"
+
 
 elif platform == "win32" or platform == "win64":
-    path = path + "\\"
+    path = runtime_path + "\\"
 
-    command_win32 = '''
+    prompt_command = '''
     set /p asker="How many users would you want?: "
     for /l {x}x in (1,1,{y}sker%) DO (wt -w 0 nt cmd /k "python {z}meet.py" timeout /t 2 /nobreak > nul)'''.format(x = r"%%", y = r"%a", z = path)
 
-    command = command_win32 #     @echo off, setlocal enableDelayedExpansion     For batch files
-    clearscreen = "cls"
+   # @echo off, setlocal enableDelayedExpansion     For batch files
 
 elif platform == "linux" or platform == "linux2":
-    path = path + "/"
+    path = runtime_path + "/"
 
-    command_linux = '''
+    prompt_command = '''
     #!/bin/bash
     echo "How many users would you want?"
     read users
     for i in $(seq $users)
     do
         echo ${x}
-        gnome-terminal --tab-with-profile=auto --title="tab$i" -e "python3 {y}meet.py"
-        sleep 2
+        gnome-terminal --tab-with-profile=auto --title="tab$i" -e "python3 {y}meet.psleep 2
 
     done'''.format(x = r"{BASH_VERSION}", y = path)
-
-    command = command_linux
-    clearscreen = "clear"
 
 else:
     exit("Not available yet.")
@@ -59,9 +49,8 @@ else:
 
 
 def names():
-        
     names_file = path + "names.csv"
-    if names_file.endswith("names.csv"):
+    if names_file: # if names_file.endswith("names.csv"):
         nameFile = open(names_file, "r")
         nameDict = csv.DictReader(nameFile, delimiter=',')
         nameList = []
@@ -70,13 +59,12 @@ def names():
         return(nameList)
     else:
         print("\nInvalid names file")
-        time.sleep(1)
-        os.system("clear")
+        sleep(1)
+        clear(command=clear_arg)
         return names()
 
 
 def details():
-
     details_file = path + "details.csv"
     if details_file.endswith("details.csv"):
         detailsFile = open(details_file, "r")
@@ -86,25 +74,25 @@ def details():
         return detailsDict
     else:
         print("\nInvalid details file")
-        time.sleep(1)
-        os.system("clear")
+        sleep(1)
+        clear(command=clear_arg)
         return details()
 
+
 def get_link():
-    os.system(f"{clearscreen}")
+    clear(command=clear_arg)
     url = input("Paste in your meeting link: ").strip()
     if url.startswith("https://meet.google.com/"):
         return url
     else:
         print("Invalid meeting link")
-        time.sleep(1)
+        sleep(1)
         return get_link()
 
 
 def times():
-
     try:
-        os.system(f"{command}")
+        os.system(prompt_command)
     except ValueError:
         return times()
 
@@ -116,14 +104,11 @@ except:
     print("Invalid files path")
     exit("Correct the file paths then run program again")
 
-try:
-    link = get_link()
-except:
-    exit()
-
 
 if __name__ == "__main__":
-    os.system(f"{clearscreen}")
+    clean_up()
+    link = get_link()
+    clear(command=clear_arg)
     times()
     
 
